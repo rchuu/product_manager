9c88a48c2ba2e15b9c4ba1f8ac2da7d995957d1c
Richard Chu <rchu1211@gmail.com>
2022-06-23 16:29:44 -0700

    updated with edit and delete

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,7 +1,9 @@
 const ProductController = require('../controllers/product.controller');
 module.exports = function (app) {
-    app.get('/api', ProductController.index); // this is the route that is called when the client makes a GET request to the server
-    app.post('/api/products', ProductController.createProduct); // this is the route that is called when the client makes a POST request to the server
-    app.get('/api/products', ProductController.getAllProducts); // this is the route that is called when the client makes a GET request to the server
-    app.get('/api/products/:id', ProductController.getProduct); // this is the route that is called when the client makes a GET request to the server
+    app.get('/api', ProductController.index);
+    app.post('/api/products', ProductController.createProduct);
+    app.get('/api/products', ProductController.getAllProducts);
+    app.get('/api/products/:id', ProductController.getProduct);
+    app.put('/api/products/:id', ProductController.updateProduct);
+    app.delete('/api/products/:id', ProductController.deleteProduct);
 }
